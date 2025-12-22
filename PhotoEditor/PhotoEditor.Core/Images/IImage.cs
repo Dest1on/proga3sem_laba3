82f7d4d5500b4f11b014240bccb3ce1b98069966
Dmitry Dem <dimixlive87@gmail.com>
2025-12-22 13:40:13 +0700

    + in-memory image , fix IImage

--- PhotoEditor/PhotoEditor.Core/Images/IImage.cs
+++ PhotoEditor/PhotoEditor.Core/Images/IImage.cs
@@ -3,14 +3,13 @@ using System.Drawing;
 namespace PhotoEditor.Core.Images
 {
     /// Абстракция изображения.
-    public interface IImage
-    {
-        int Width { get; }
-        int Height { get; }
+public interface IImage
+{
+    int Width { get; }
+    int Height { get; }
 
-        Color GetPixel(int x, int y);
-        void SetPixel(int x, int y, Color color);
+    Color GetPixel(int x, int y);
+    void SetPixel(int x, int y, Color color);
+}
 
-        Bitmap ToBitmap();
-    }
 }
