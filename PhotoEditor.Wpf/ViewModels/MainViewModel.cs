dade0727763a0b9714d27ca77fd8dbf72e128695
Dmitry Dem <dimixlive87@gmail.com>
2025-12-27 00:11:46 +0700

    Fix WPF to Core PixelColor conversion

--- PhotoEditor.Wpf/ViewModels/MainViewModel.cs
+++ PhotoEditor.Wpf/ViewModels/MainViewModel.cs
@@ -66,8 +66,7 @@ namespace PhotoEditor.Wpf.ViewModels
             dialog.Filter = "Images(*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg";
 
             bool? result = dialog.ShowDialog();
-
-            if (result!=true) return;
+            if (result != true) return;
 
             BitmapImage bitmap = new BitmapImage();
 
@@ -82,13 +81,14 @@ namespace PhotoEditor.Wpf.ViewModels
             bitmap.Freeze();
 
             InMemoryImage image = new InMemoryImage(bitmap.PixelWidth, bitmap.PixelHeight);
+
             int stride = bitmap.PixelWidth * 4;
             byte[] pixels = new byte[bitmap.PixelHeight * stride];
             bitmap.CopyPixels(pixels, stride, 0);
 
             for (int y = 0; y < bitmap.PixelHeight; y++)
             {
-               for (int x = 0; x < bitmap.PixelWidth; x++)
+                for (int x = 0; x < bitmap.PixelWidth; x++)
                 {
                     int index = y * stride + x * 4;
 
@@ -96,14 +96,15 @@ namespace PhotoEditor.Wpf.ViewModels
                     byte g = pixels[index + 1];
                     byte r = pixels[index + 2];
                     byte a = pixels[index + 3];
-                    
-                    image.SetPixel(x, y, Color.FromArgb(a, r, g, b));
 
-                } 
-            }
+                    image.SetPixel(x, y, new PixelColor(r, g, b, a));
+                  }
+           }
+
             _currentImage = image;
             CurrentBitmap = _imageAdapter.Convert(_currentImage);
         }
+
 
         private void Crop()
         {
